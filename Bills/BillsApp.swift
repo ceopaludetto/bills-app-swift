251f2f9c87034d9eef16bf8fc2afe5010fe9d603
Carlos Eduardo <ceo.paludetto@gmail.com>
2021-09-24 18:48:32 -0300

    Initial Commit

---
+++ Bills/BillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillsApp.swift
+//  Bills
+//
+//  Created by Carlos Eduardo on 24/09/21.
+//
+
+import SwiftUI
+
+@main
+struct BillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
